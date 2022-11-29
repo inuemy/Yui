898c3bc30b4ac865109c6773c434258323ef7ebe
MayuInoue <107013381+inuemy@users.noreply.github.com>
2022-11-29 11:09:37 +0000

    add customer params

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -21,7 +21,7 @@ class Admin::CustomersController < ApplicationController
 
   private
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :nickname, :sex, :birthday, :prefecture_id, :email, :is_deleted)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :sex, :birthday, :prefecture_id, :email, :is_deleted)
   end
 
 end
