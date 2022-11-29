0a93e5c02097f6546461806d1902f27e9a08ba01
MayuInoue <107013381+inuemy@users.noreply.github.com>
2022-11-29 10:12:03 +0000

    bug fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -41,7 +41,7 @@ class Customer < ApplicationRecord
 
   def get_profile_image
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
       profile_image
